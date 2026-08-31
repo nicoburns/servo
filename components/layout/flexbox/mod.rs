99faaf92b5f484e23e9989f46e06f29d511e2f3c
Nico Burns <nico@nicoburns.com>
2026-08-31 21:31:42 +0100

    Upgrade to Stylo with flex-wrap:balance parsing support
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- components/layout/flexbox/mod.rs
+++ components/layout/flexbox/mod.rs
@@ -51,18 +51,12 @@ impl FlexContainerConfig {
         let flex_direction = container_style.clone_flex_direction();
         let flex_axis = FlexAxis::from(flex_direction);
         let flex_wrap = container_style.get_position().flex_wrap;
-        let container_is_single_line = match flex_wrap {
-            FlexWrap::Nowrap => true,
-            FlexWrap::Wrap | FlexWrap::WrapReverse => false,
-        };
+        let container_is_single_line = matches!(flex_wrap, FlexWrap::NOWRAP);
         let flex_direction_is_reversed = match flex_direction {
             FlexDirection::Row | FlexDirection::Column => false,
             FlexDirection::RowReverse | FlexDirection::ColumnReverse => true,
         };
-        let flex_wrap_reverse = match flex_wrap {
-            FlexWrap::Nowrap | FlexWrap::Wrap => false,
-            FlexWrap::WrapReverse => true,
-        };
+        let flex_wrap_reverse = flex_wrap.contains(FlexWrap::WRAP_REVERSE);
 
         let align_content = container_style.clone_align_content();
         let align_items = AlignItems(match container_style.clone_align_items().0 {
